93fd93e073687f5fb1ac352331397f53cc803942
Douglas Lyman <douglas.lyman@icloud.com>
2023-11-16 10:28:52 +0100

    Finished Day 3 Part 2

--- Day 3/2.c
+++ Day 3/2.c
@@ -38,17 +38,22 @@ int main() {
     CHAR szBackpacks[3][64];
 
     while (!feof(pFile)) {
-        for (INT i = 0; i < 3; i++)
-            fscanf_s(pFile, "%s\n", szBackpacks[i], sizeof(szBackpacks[i]));
-        
-        for (INT i = 0; i < sizeof(szBackpacks[0]); i++)
-            for (INT j = 0; j < sizeof(szBackpacks[1]); j++)
+        fscanf_s(pFile, "%s\n%s\n%s\n",
+            szBackpacks[0], sizeof(szBackpacks[0]),
+            szBackpacks[1], sizeof(szBackpacks[1]),
+            szBackpacks[2], sizeof(szBackpacks[2])
+        );
+
+        for (INT i = 0; i < strlen(szBackpacks[0]); i++)
+            for (INT j = 0; j < strlen(szBackpacks[1]); j++)
                 if (szBackpacks[0][i] == szBackpacks[1][j] && !IsCommon(szBackpacks[0][i]))
                     AddCommon(szBackpacks[0][i]);
 
-        for (INT i = 0; i < sizeof(szBackpacks[2]); i++)
-            if (IsCommon(szBackpacks[2][i]))
+        for (INT i = 0; i < strlen(szBackpacks[2]); i++)
+            if (IsCommon(szBackpacks[2][i])) {
                 nSum += GetPriority(szBackpacks[2][i]);
+                break;
+            }
 
         ResetCommon();
     }
