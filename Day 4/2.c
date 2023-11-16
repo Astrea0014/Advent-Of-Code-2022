fc0ac000ad89f67f86538c847bba345542668b60
Douglas Lyman <douglas.lyman@icloud.com>
2023-11-16 11:04:38 +0100

    Completed Day 4 Part 2

--- Day 4/2.c
+++ Day 4/2.c
@@ -3,5 +3,22 @@
 #include <Windows.h>
 
 int main() {
-    
+    FILE* pFile;
+    fopen_s(&pFile, "input.txt", "r");
+
+    INT nCount = 0;
+
+    INT nFirstLower, nFirstHigher, nSecondLower, nSecondHigher;
+
+    while (!feof(pFile)) {
+        fscanf_s(pFile, "%i-%i,%i-%i\n", &nFirstLower, &nFirstHigher, &nSecondLower, &nSecondHigher);
+
+        // Scenarios:
+        // First lower must be bigger than second higher or vice versa.
+
+        if (!(nFirstLower > nSecondHigher || nFirstHigher < nSecondLower))
+            nCount++;
+    }
+
+    printf("Count: %i\n", nCount);
 } 
